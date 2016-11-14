cf8b2dd254b3e95ae4f3655d7120b40a8bcd8fb8
linsama <jianglin04@baidu.com>
2016-11-14 20:54:49 +0800

    add action handler

--- run_test.py
+++ run_test.py
@@ -3,14 +3,18 @@
 
 from src.case_handler import *
 from src.api_case_handler import *
+from src.action_handler import *
 
 
 def main():
     case_handler = CaseHandler()
     api_cases = case_handler.return_api_cases()
     base_config = case_handler.return_base_config()
+
     api_case_handler = ApiCaseHandler(base_config["host"], api_cases)
     api_case_handler.run_test()
+
+    action_handler = ActionHandler()
 
 
 if __name__ == '__main__':
