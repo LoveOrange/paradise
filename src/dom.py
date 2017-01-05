f55a9b001f9585125c444e1a33d8f2f58292d868
linsama <orange24k@hotmail.com>
2017-01-06 01:05:36 +0800

    add dom parser

--- src/dom.py
+++ src/dom.py
@@ -1,2 +1,37 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
+
+def get_type(value):
+    if value.startswith("."):
+        return "class"
+    elif value.startswith("#"):
+        return "id"
+    else:
+        return "div"
+
+
+def get_dom(value):
+    if value.startswith(".") or value.startswith("#"):
+        value = value[1:]
+    if "(" in value:
+        return value[:value.index("(")]
+    else:
+        return value
+
+
+def get_params(value):
+    if "(" in value:
+        return value[value.index("(") + 1:-1]
+    return ""
+
+
+class Dom:
+    """
+    Dom obj, for dom dsl
+    """
+    def __init__(self, value):
+        self.value = value
+        self.type = get_type(value)
+        self.dom = get_dom(value)
+        self.params = get_params(value)
--- src/selenium_utils.py
+++ src/selenium_utils.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+from dom import Dom
 from selenium import webdriver
 
 
 class SeleniumUtils(object):
 
     def __init__(self):
-        print "init"
         self._browser = webdriver.Chrome()
 
     def check_title(self, expect):
@@ -17,10 +17,19 @@ class SeleniumUtils(object):
     def click(self, dom):
         self._browser.get("https://baidu.com")
 
-    def fill(self, dom, value):
-        _dom = self._browser.find_element_by_class_name(dom)
-        _dom.send_keys(value)
-        print 'I love chengzi'
+    def fill(self, dom):
+        dom_element = Dom(dom)
+        fill_value = dom.params
+        _dom = self.get_dom_element(dom_element)
+        _dom.send_keys(fill_value)
+
+    def get_dom_element(self, dom):
+        if dom.type == "class":
+            return self._browser.find_element_by_class_name(dom.dom)
+        elif dom.type == "id":
+            return self._browser.find_element_by_id(dom.dom)
+        else:
+            return self._browser.find_element_by_tag_name(dom.dom)
 
 
 browser = None
